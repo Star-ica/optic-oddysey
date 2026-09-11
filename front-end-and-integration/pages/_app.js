5e562c5956b82d549483499a84387877330217a3
akangbeabigael11@gmail.com <jerydan148@gmail.com>
2026-09-11 14:42:52 +0100

    first commit

---
+++ front-end-and-integration/pages/_app.js
@@ -0,0 +1,20 @@
+import '@/styles/globals.css'
+import { Web3Modal } from '@/context/web3modal';
+import DisableRightClick from '@/components/disablerightclick';
+import DisableDrag from '@/components/disabledrag';
+export const metadata = {
+  title: 'Optic Odyssey',
+  description: 'Explore, discover NFTs and make your photos work for you on BNB Smart Chain'
+}
+
+export default function App({ Component, pageProps }) {
+  return  (
+         <>
+           <Web3Modal>
+           <DisableRightClick />
+           <DisableDrag /> 
+           <Component {...pageProps} />
+           </Web3Modal>
+          </>
+          )
+}
